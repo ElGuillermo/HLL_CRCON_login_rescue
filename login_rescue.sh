c75c237ca1770229ed567c720e0c3bffaaab8fc6
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-12-08 12:39:23 +0100

    Update login_rescue.sh

--- login_rescue.sh
+++ login_rescue.sh
@@ -10,16 +10,16 @@
 # - Some Ubuntu distros disable 'root' user,
 #   you may have installed CRCON in "/home/ubuntu/hll_rcon_tool" then.
 # default : "/root/hll_rcon_tool"
-CRCON_folder_path="/root/hll_rcon_tool"
+CRCON_folder_path=""
 
 # Set to "yes" if you have modified any file that comes from CRCON repository
-# First build will take ~3-4 minutes. Subsequent ones will take ~30 seconds.
+# First build will take ~3-4 minutes. Subsequent ones will take ~20 seconds.
 # Default : "yes"
 rebuild_before_restart="yes"
 
 # Create a "rescue" CRCON superuser
 # Its default password will be "helpmeplease"
-# Don't forget to disable/delete this user after your maintenance operation
+# Don't forget to disable/delete this user after your maintenance operation !
 # Default : "no"
 create_superuser="no"
 
@@ -27,59 +27,81 @@ create_superuser="no"
 # It must match the HLL_DB_PASSWORD= value set in your .env file
 # Default : "no"
 change_db_pwd="no"
-new_db_pwd=""  # Could be any non blank string without a space or a % sign
+new_db_pwd=""  # Could be any non-blank string without a space or a % sign
 #
 # └───────────────────────────────────────────────────────────────────────────┘
+
+is_CRCON_configured() {
+  printf "%s└ \033[34m?\033[0m Testing folder : \033[33m%s\033[0m\n" "$2" "$1"
+  if [ -f "$1/compose.yaml" ] && [ -f "$1/.env" ]; then
+    printf "%s  └ \033[32mV\033[0m A valid CRCON install has been found in \033[33m%s\033[0m\n" "$2" "$1"
+  else
+    if [ ! -f "$1/.env" ]; then
+      printf "%s  └ \033[31mX\033[0m Missing file : '\033[37m.env\033[0m'\n" "$2"
+      printf "\n\033[32mWhat to do\033[0m :\nFollow the install procedure to create a '\033[37m.env\033[0m' file\n\n"
+    fi
+    if [ ! -f "$1/compose.yaml" ]; then
+      printf "%s  └ \033[31mX\033[0m Missing file : '\033[37mcompose.yaml\033[0m'\n" "$2"
+      if [ -f "$1/compose.yml" ]; then
+        printf "%s  └ \033[31m!\033[0m Deprecated file found : '\033[37mcompose.yml\033[0m'\n" "$2"
+      fi
+      if [ -f "$1/docker-compose.yml" ]; then
+        printf "%s  └ \033[31m!\033[0m Deprecated file found : '\033[37mdocker-compose.yml\033[0m'\n" "$2"
+      fi
+      printf "\n\033[32mWhat to do\033[0m :\nFollow the install procedure to create a '\033[37mcompose.yaml\033[0m' file\n\n"
+    fi
+    exit
+  fi
+}
 
 clear
 printf "┌─────────────────────────────────────────────────────────────────────────────┐\n"
 printf "│ Login rescue                                                                │\n"
 printf "└─────────────────────────────────────────────────────────────────────────────┘\n\n"
 
-# User must have root permissions
+this_script_dir=$(dirname -- "$( readlink -f -- "$0"; )";)
 this_script_name=${0##*/}
+
+# User must have root permissions
 if [ "$(id -u)" -ne 0 ]; then
   printf "\033[31mX\033[0m This \033[37m%s\033[0m script must be run with full permissions\n\n" "$this_script_name"
   printf "\033[32mWhat to do\033[0m : you must elevate your permissions using 'sudo' :\n"
   printf "\033[36msudo sh ./%s\033[0m\n\n" "$this_script_name"
   exit
+# Root
 else
   printf "\033[32mV\033[0m You have 'root' permissions.\n"
 fi
 
 # Check CRCON folder path
 if [ -n "$CRCON_folder_path" ]; then
-  crcon_dir=$CRCON_folder_path
   printf "\033[32mV\033[0m CRCON folder path has been set in config : \033[33m%s\033[0m\n" "$CRCON_folder_path"
+  is_CRCON_configured "$CRCON_folder_path" ""
+  crcon_dir="$CRCON_folder_path"
 else
-  printf "\033[34m?\033[0m You didn't set any CRCON folder path in config\n"
-  printf "  Trying to detect a \033[33mhll_rcon_tool\033[0m folder...\n"
-  crcon_dir=$(find / -name "hll_rcon_tool" 2>/dev/null)
-  if [ -n "$crcon_dir" ]; then
-    printf "\033[32mV\033[0m CRCON folder detected in \033[33m%s\033[0m\n" "$crcon_dir"
+  printf "\033[31mX\033[0m You didn't set any CRCON folder path in config\n"
+  printf "└ \033[34m?\033[0m Trying to detect a \033[33mhll_rcon_tool\033[0m folder\n"
+  detected_dir=$(find / -name "hll_rcon_tool" 2>/dev/null)
+  if [ -n "$detected_dir" ]; then
+    is_CRCON_configured "$detected_dir" "  "
+    crcon_dir="$detected_dir"
   else
-    printf "\033[31mX\033[0m No \033[33mhll_rcon_tool\033[0m folder could be found\n\n"
-    printf "  - Maybe you renamed the \033[33mhll_rcon_tool\033[0m folder ?\n"
-    printf "    (it will work the same, but you'll have to adapt every maintenance script)\n\n"
-    printf "  If you followed the official install procedure,\n"
-    printf "  your \033[33mhll_rcon_tool\033[0m folder should be found here :\n"
-    printf "    - \033[33m/root/hll_rcon_tool\033[0m        (most Linux installs)\n"
-    printf "    - \033[33m/home/ubuntu/hll_rcon_tool\033[0m (some Ubuntu installs)\n\n"
-    printf "\033[32mWhat to do\033[0m :Find your CRCON folder, copy this script in it and relaunch it from there.\n\n"
-    exit
+    printf "  └ \033[31mX\033[0m No \033[33mhll_rcon_tool\033[0m folder could be found\n"
+    printf "    └ \033[34m?\033[0m Trying to detect a CRCON install in current folder\n"
+    is_CRCON_configured "$this_script_dir" "      "
+    crcon_dir="$this_script_dir"
   fi
 fi
 
-# Script has to be in the CRCON folder
-this_script_dir=$(dirname -- "$( readlink -f -- "$0"; )";)
+# This script has to be in the CRCON folder
 if [ ! "$this_script_dir" = "$crcon_dir" ]; then
   printf "\033[31mX\033[0m This script is not located in the CRCON folder\n"
   printf "  Script location : \033[33m%s\033[0m\n" "$this_script_dir"
   printf "  Should be here : \033[33m%s\033[0m\n" "$crcon_dir"
-  printf "\033[32mFixing...\033[0m\n"
+  printf "  \033[32mTrying to fix...\033[0m\n"
   cp "$this_script_dir/$this_script_name" "$crcon_dir"
   if [ -f "$crcon_dir/$this_script_name" ]; then
-    printf "\033[32mV\033[0m \033[37m%s\033[0m has been copied in \033[33m%s\033[0m\n\n" "$this_script_name" "$crcon_dir"
+    printf "  \033[32mV\033[0m \033[37m%s\033[0m has been copied in \033[33m%s\033[0m\n\n" "$this_script_name" "$crcon_dir"
     printf "\033[32mWhat to do\033[0m : enter the CRCON folder and relaunch the script using this command :\n"
     printf "\033[36mrm %s && cd %s && sudo sh ./%s\033[0m\n\n" "$this_script_dir/$this_script_name" "$crcon_dir" "$this_script_name"
     exit
@@ -95,27 +117,12 @@ fi
 # Script has to be launched from CRCON folder
 current_dir=$(pwd | tr -d '\n')
 if [ ! "$current_dir" = "$crcon_dir" ]; then
-  printf "\033[31mX\033[0m This \033[37m%s\033[0m script should be run from the CRCON folder\n\n" "$this_script_name"
+  printf "\033[31mX\033[0m This script should be run from the CRCON folder\n\n"
   printf "\033[32mWhat to do\033[0m : enter the CRCON folder and relaunch the script using this command :\n"
   printf "\033[36mcd %s && sudo sh ./%s\033[0m\n\n" "$crcon_dir" "$this_script_name"
   exit
 else
   printf "\033[32mV\033[0m This script has been run from the CRCON folder\n"
-fi
-
-# CRCON config check
-if [ ! -f "$crcon_dir/compose.yaml" ] || [ ! -f "$crcon_dir/.env" ]; then
-  printf "\033[31mX\033[0m CRCON doesn't seem to be configured\n"
-  if [ ! -f "$crcon_dir/compose.yaml" ]; then
-    printf "  \033[31mX\033[0m There is no '\033[37mcompose.yaml\033[0m' file in \033[33m%s\033[0m\n" "$crcon_dir"
-  fi
-  if [ ! -f "$crcon_dir/.env" ]; then
-    printf "  \033[31mX\033[0m There is no '\033[37m.env\033[0m' file in \033[33m%s\033[0m\n" "$crcon_dir"
-  fi
-  printf "\n\033[32mWhat to do\033[0m : check your CRCON install in \033[33m%s\033[0m\n\n" "$crcon_dir"
-  exit
-else
-  printf "\033[32mV\033[0m CRCON seems to be configured\n"
 fi
 
 printf "\033[32mV Everything's fine\033[0m Let's renew these passwords !\n\n"
@@ -175,6 +182,7 @@ if [ $change_db_pwd = "yes" ]; then
           docker compose up -d postgres
           printf "Setting new database password\n"
           docker compose exec -it postgres psql -U rcon -c "ALTER USER rcon WITH PASSWORD '$new_db_pwd';"
+          printf "Here live dragons\n"
           printf "Stopping postgres Docker container\n"
           docker compose down
           db_pwd_updated="yes"
